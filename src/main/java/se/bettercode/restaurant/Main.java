37d1c82733b60c0cde89c5689745e6feb143132a
Max Wenzin <max.wenzin@gmail.com>
2017-04-09 22:25:16 +0200

    Added ability to have different XML menues, and chose from command line.

--- src/main/java/se/bettercode/restaurant/Main.java
+++ src/main/java/se/bettercode/restaurant/Main.java
@@ -5,7 +5,12 @@ import java.io.IOException;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    new MenuPrinter().print();
+    String file = parseArgument(args);
+    new MenuPrinter(file).print();
+  }
+
+  private static String parseArgument(String[] args) {
+    return args.length > 0 ? args[0] : "default-menu.xml";
   }
 
 }
--- src/main/java/se/bettercode/restaurant/MenuPrinter.java
+++ src/main/java/se/bettercode/restaurant/MenuPrinter.java
@@ -1,14 +1,20 @@
 package se.bettercode.restaurant;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import se.bettercode.restaurant.model.BreakfastMenu;
 import se.bettercode.restaurant.model.Food;
 
 import java.io.IOException;
 
+@AllArgsConstructor
 public class MenuPrinter {
 
+  @Getter
+  private String file;
+
   public void print() throws IOException {
-    final String xml = new XmlFileLoader().loadFileAsString("menu.xml");
+    final String xml = new XmlFileLoader().loadFileAsString(getFile());
     final BreakfastMenu breakfastMenu = new BreakfastMenuMaker().fromXmlString(xml);
     printMenu(breakfastMenu);
   }
