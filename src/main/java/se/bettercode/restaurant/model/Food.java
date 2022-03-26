7bfd1ca8cbdb7de4a461d273267f5911a8f6f9a5
Max Wenzin <max.wenzin@gmail.com>
2022-03-26 12:00:33 +0100

    Upgraded gradle wrapper: gradlew wrapper --gradle-version 7.4.1

--- src/main/java/se/bettercode/restaurant/model/Food.java
+++ src/main/java/se/bettercode/restaurant/model/Food.java
@@ -3,9 +3,11 @@ package se.bettercode.restaurant.model;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Food {
 
   @JacksonXmlProperty
